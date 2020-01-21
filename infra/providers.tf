0bec4f255012ae90d8ae5e51f65daf894803a174
Can Elkatmış <canelkatmis@gmail.com>
2020-01-21 12:59:02 +0300

    Create providers.tf

---
+++ infra/providers.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region  = "eu-central-1"
+  version = ">= 2.38.0"
+}
+
+data "aws_region" "current" {}
+
+data "aws_availability_zones" "available" {}
+
+# Not required: currently used in conjuction with using
+# icanhazip.com to determine local workstation external IP
+# to open EC2 Security Group access to the Kubernetes cluster.
+# See workstation-external-ip.tf for additional information.
+provider "http" {}
