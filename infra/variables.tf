199f43bcf75e5e0a51c3d04e4151b8d5f51f6cf1
Can Elkatmış <canelkatmis@gmail.com>
2020-01-21 13:11:59 +0300

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -6,3 +6,13 @@ variable "cluster-name" {
   default = "terraform-eks-demo"
   type    = string
 }
+
+variable "tf-cloud-org" {
+  default = "canelkatmis"
+  type    = string
+}
+
+variable "tf-cloud-workspace" {
+  default = "eks-demo"
+  type    = string
+}
